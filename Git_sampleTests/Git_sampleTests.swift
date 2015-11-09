9dacbd6145361735031f575dd0efc26578efe3f7
yusuke_kimoto <yusuke_kimoto@mokuhon-no-MacBook-Air.local>
2015-11-09 18:14:29 +0900

    初コミット

---
+++ Git_sampleTests/Git_sampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  Git_sampleTests.swift
+//  Git_sampleTests
+//
+//  Created by Soya Takahashi on 2015/10/07.
+//  Copyright © 2015年 TECH::CAMP. All rights reserved.
+//
+
+import XCTest
+@testable import Git_sample
+
+class Git_sampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
